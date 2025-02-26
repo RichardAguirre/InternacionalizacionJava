1c379ca0c7ffc2523e8d93e13a8cb0544c583366
Richard Aguirre Correa <richard.aguirre@inmel.co>
2025-02-26 11:30:07 -0500

    internacionalizacion de mensajes

--- src/main/java/com/example/parcial2/controllers/SaludoController.java
+++ src/main/java/com/example/parcial2/controllers/SaludoController.java
@@ -2,14 +2,22 @@ package com.example.parcial2.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api")
 public class SaludoController {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/saludo")
-    public String obtenerSaludo() {
-        return "¡Hola, API RESTful!";
+    public String obtenerSaludo(@RequestParam(name = "lang", defaultValue = "es") String lang) {
+        Locale locale = "en".equals(lang) ? Locale.ENGLISH : new Locale("es");
+        return messageSource.getMessage("saludo", null, locale);
     }
 }
